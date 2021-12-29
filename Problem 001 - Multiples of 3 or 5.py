62896d91986756f44739da4cdb049de856792107
Dan Kellett <bobbins3000@hotmail.co.uk>
2021-12-29 16:42:31 +0000

    Rename p2 to p002 and add commentary

--- Problem 001 - Multiples of 3 or 5.py
+++ Problem 001 - Multiples of 3 or 5.py
@@ -1,6 +1,6 @@
 # Project Euler: Problem 1
 # Multiples of 3 or 5
-# Uses: Loops, modulo
+# Uses: Loops, modulo, if, print
 
 # Problem set-up
 # If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
